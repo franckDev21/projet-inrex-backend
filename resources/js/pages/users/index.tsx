936f7981caa87c4683533ad50957b0d8b17d56bf
franckDev21 <tutodevphp@gmail.com>
2025-06-05 03:01:36 +0100

    first

---
+++ resources/js/pages/users/index.tsx
@@ -0,0 +1,21 @@
+import AppLayout from '@/layouts/app-layout';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/react';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'User Management',
+        href: '/users',
+    },
+];
+
+export default function Dashboard() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Utilisateurs" />
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+                Hello, this is the user management page. You can manage users here.
+            </div>
+        </AppLayout>
+    );
+}
