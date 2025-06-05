0b875d330f1c6feb90ed4f5a3831dd14dc3bd58f
franckDev21 <tutodevphp@gmail.com>
2025-06-05 16:03:00 +0100

    update

--- app/Http/Controllers/Api/Auth/LoginCustomerController.php
+++ app/Http/Controllers/Api/Auth/LoginCustomerController.php
@@ -40,7 +40,7 @@ class LoginCustomerController extends Controller
         $token = $customer->createToken('api-token')->plainTextToken; // Removed 'role:customer' ability
 
         return response()->json([
-            'message' => 'Customer logged in successfully2.',
+            'message' => 'Customer logged in successfully.',
             'customer' => $customer->only(['id', 'name', 'email']),
             'token' => $token,
         ]);
