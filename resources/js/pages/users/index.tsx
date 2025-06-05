9b41bc4efcba1990f81aab462cb1558fc5b2bcda
franckDev21 <tutodevphp@gmail.com>
2025-06-05 05:10:11 +0100

    update: users/index.ts, test deploy

--- resources/js/pages/users/index.tsx
+++ resources/js/pages/users/index.tsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Utilisateurs" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                Hello, this is the user management page. You can manage users here.
+                Hello - UPDATE, this is the user management page. You can manage users here.
             </div>
         </AppLayout>
     );
